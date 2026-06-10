d8969e63788c126813a769439f0596ae457a8637
ivood <ivood@Death-Star>
2026-06-10 17:15:14 -0300

    primer commit

---
+++ src/test/java/tuti/desi/TpIntegradorProbaReiniciandoApplicationTests.java
@@ -0,0 +1,13 @@
+package tuti.desi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradorProbaReiniciandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
