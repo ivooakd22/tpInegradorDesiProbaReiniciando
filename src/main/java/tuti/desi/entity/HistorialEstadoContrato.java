95df580694c7bd3059899ee4296db6b3ddea0a91
FlorenciaNieva <florencianieva930@gmail.com>
2026-06-30 19:28:50 -0300

    Agrego modificaciones

---
+++ src/main/java/tuti/desi/entity/HistorialEstadoContrato.java
@@ -0,0 +1,53 @@
+import jakarta.persistence.*;
+import java.time.LocalDate;
+ 
+@Entity
+public class HistorialEstado {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    
+    @ManyToOne
+    @JoinColumn(name = "contrato_id", nullable = false)
+    private Contrato contrato;
+ 
+    @Enumerated(EnumType.STRING)
+    private EstadoContrato estado;
+ 
+    private LocalDate fechaCambio;
+ 
+    
+    public Long getId() {
+        return id;
+    }
+ 
+    public void setId(Long id) {
+        this.id = id;
+    }
+ 
+    public Contrato getContrato() {
+        return contrato;
+    }
+ 
+    public void setContrato(Contrato contrato) {
+        this.contrato = contrato;
+    }
+ 
+    public EstadoContrato getEstado() {
+        return estado;
+    }
+ 
+    public void setEstado(EstadoContrato estado) {
+        this.estado = estado;
+    }
+ 
+    public LocalDate getFechaCambio() {
+        return fechaCambio;
+    }
+ 
+    public void setFechaCambio(LocalDate fechaCambio) {
+        this.fechaCambio = fechaCambio;
+    }
+}
